50b3f4db1b8fd7a1c7a388e26e41179f47a104bf
drmbiomechanics <danmartel11@hotmail.com>
2025-11-21 12:40:48 -0500

    Updated score thresholds to be more linear

--- scripts/score_screen.gd
+++ scripts/score_screen.gd
@@ -9,27 +9,27 @@ func _ready():
 	score = GlobalVariables.game_score
 	update_score()
 	
-	if score < 600:
+	if score < 1000:
 		$Requested_Wave_Node/Swimmer_Rank_1_Left.show()
 		$Requested_Wave_Node/Swimmer_Rank_1_Right.show()
 		$Rank_Container/Rank_Container6/Rank_6_Label.add_theme_color_override("font_color",Color(r,g,b,1))
-	elif score < 1500:
+	elif score < 2000:
 		$Requested_Wave_Node/Swimmer_Rank_2_Left.show()
 		$Requested_Wave_Node/Swimmer_Rank_2_Right.show()
 		$Rank_Container/Rank_Container5/Rank_5_Label.add_theme_color_override("font_color",Color(r,g,b,1))
-	elif score < 3500:
+	elif score < 3000:
 		$Requested_Wave_Node/Swimmer_Rank_3_Left.show()
 		$Requested_Wave_Node/Swimmer_Rank_3_Right.show()
 		$Rank_Container/Rank_Container4/Rank_4_Label.add_theme_color_override("font_color",Color(r,g,b,1))
-	elif score < 5000:
+	elif score < 4000:
 		$Requested_Wave_Node/Swimmer_Rank_4_Left.show()
 		$Requested_Wave_Node/Swimmer_Rank_4_Right.show()
 		$Rank_Container/Rank_Container3/Rank_3_Label.add_theme_color_override("font_color",Color(r,g,b,1))
-	elif score < 5900:
+	elif score < 5000:
 		$Requested_Wave_Node/Swimmer_Rank_5_Left.show()
 		$Requested_Wave_Node/Swimmer_Rank_5_Right.show()
 		$Rank_Container/Rank_Container2/Rank_2_Label.add_theme_color_override("font_color",Color(r,g,b,1))
-	elif score >= 5900:
+	elif score >= 5000:
 		$Requested_Wave_Node/Swimmer_Rank_6_Left.show()
 		$Requested_Wave_Node/Swimmer_Rank_6_Right.show()
 		$Rank_Container/Rank_Container1/Rank_1_Label.add_theme_color_override("font_color",Color(r,g,b,1))
